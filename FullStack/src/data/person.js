cef4d766368beeeeca33c66811052cc1b4e60ae2
PabloKinsel <pabloyurik@gmail.com>
2023-05-11 16:53:08 -0300

    TrabalhoFullStack

---
+++ FullStack/src/data/person.js
@@ -0,0 +1,18 @@
+
+const Person = {
+  name: 'Pablo Y Kinsel',
+  office:"Web Developer",
+  file: {
+    cover:"https://images.unsplash.com/photo-1607743386760-88ac62b89b8a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=350&q=80",
+    avatar:"https://github.com/PabloKinsel.png"
+  },
+  stacks: [
+    { Tec: "JavaScript", color: "#ff530f" },
+    { Tec: "HTML", color: "#ff530f"},
+    { Tec: "CSS", color: "#ff530f" },
+    { Tec: "Typescript", color: "#ff530f" },
+    { Tec: "React", color: "#ff530f" }
+  ]
+}
+
+export default Person
